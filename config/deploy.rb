247d10c40284d341e7710937f53ba92d46d7b32b
Kevin O'Donnell <kevino@kevino.net>
2013-02-21 22:47:26 -0500

    cap setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "OttWatch"
+set :repository,  "ssh://prodweb/mnt/git/ottwatch.git"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :homepath, "/mnt/www/ottwatch"
+#set :deploy_via, :remote_cache
+
+set :copy_exclude, [".git"]
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "prodweb"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
