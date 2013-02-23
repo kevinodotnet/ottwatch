2e804e91b6a46d92e99ab9bb4d936fc5a29cf589
Kevin O'Donnell <kevino@kevino.net>
2013-02-23 11:34:42 -0500

    lobby query by date is alive

--- bin/lobby-tweeter.php
+++ bin/lobby-tweeter.php
@@ -36,3 +36,33 @@ $html = "
 ";
 file_put_contents("test.html",$html);
 
+# invoke CURLkk
+
+$fields = array(
+  '__VIEWSTATE' => $viewstate,
+  '__EVENTVALIDATION' => $eventvalidation,
+  'ctl00$MainContent$dpFromDate_txtbox' => '18-Feb-2013',
+  'ctl00$MainContent$dpToDate_txtbox' => '22-Feb-2013',
+  'ctl00$MainContent$btnSearch' => 'Search'
+);
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+$fields_string = http_build_query($fields);
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch,CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17 OttWatch');
+$response = curl_exec($ch);
+curl_close($ch);
+
+print print_r($response);
+print "\n";
+print $response;
+print "\n";
+
+
+
+
+
+
