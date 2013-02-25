c8048ba60d8e3cb2c083816e7c89812e4d693d61
Kevin O'Donnell <kevino@kevino.net>
2013-02-24 22:56:08 -0500

    Fix include_path issue

--- bin/lobby-tweeter.php
+++ bin/lobby-tweeter.php
@@ -1,7 +1,11 @@
 <?php
 
+$dirname = `dirname $argv[0]`;
+$dirname = preg_replace("/\n/","",$dirname);
 ob_start();
-require_once('../lib/include.php');
+
+set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
+require_once('include.php');
 require_once('twitteroauth.php');
 
 # base URL of the lobbyist registry
