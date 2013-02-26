93766ef6fdf7dad7cf95ec0c5a0f12725a113668
Kevin O'Donnell <kevino@kevino.net>
2013-02-26 03:36:22 -0500

    google analytics

--- lib/include.php
+++ lib/include.php
@@ -172,4 +172,23 @@ function lobbyistSearch($name) {
   return $matches;
 }
 
+function googleAnalytics() {
+  ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-6324294-24']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  <?php
+
+}
+
 ?>
