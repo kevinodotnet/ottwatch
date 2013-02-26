9a60670dd446ad136a01a3e1e3d0cf1d30b52985
Kevin O'Donnell <kevino@kevino.net>
2013-02-26 03:11:29 -0500

    Fancy website

--- www/index.php
+++ www/index.php
@@ -21,21 +21,24 @@ function home() {
   function lobbyist_search_form_submit() {
     v = document.getElementById('lobbyist_search_value').value;
     if (v == '') {
+      alert('Cannot perform an empty search');
       return;
     }
     document.location.href = 'lobbyist/search/'+v;
   }
   </script>
 
+  <p/>
+
   <div class="row-fluid">
-  <span class="span6">
-  Lobbyist Search by name
+  <span class="span9">
+  <h3>Search</h3>
+  <input type="text" id="lobbyist_search_value" placeholder="Lobbyist name..."><br/>
+  <button clas="btn" onclick="lobbyist_search_form_submit()"><i class="icon-search"></i> Search</button>
   </span>
-  </div>
-  <div class="row-fluid">
-  <span class="span6">
-  <input type="text" id="lobbyist_search_value"/>
-  <input type="button" onclick="lobbyist_search_form_submit()" value="Search"/>
+  <span class="span3">
+  <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/ottwatch" data-widget-id="306310112971210752">Tweets by @ottwatch</a>
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </span>
   </div>
 
@@ -48,6 +51,7 @@ function lobbyist_search($name) {
   $matches = lobbyistSearch($name);
   $vs = $matches["__vs"]; unset($matches["__vs"]);
   $ev = $matches["__ev"]; unset($matches["__ev"]);
+
 #  if (count($matches) == 1) {
 #    reset($matches);
 #    $name = key($matches);
@@ -55,17 +59,24 @@ function lobbyist_search($name) {
 #    return;
 #  }
   if (count($matches) == 0) {
-    print "No matches\n";
+    ?>
+    <div class="row-fluid">
+    <span class="span12">
+    <h3>No Matches</h3>
+    </span>
+    </div>
+    <?php
     bottom();
     return;
   }
   ?>
-  <p>Found <?php print count($matches); ?> matches.</p>
-  <div class="span6">
+  <div class="row-fluid">
+  <span class="span3">
+  <h3>Found <?php print count($matches); ?> matches.</h3>
   <table class="table table-hover table-condensed">
   <tr>
-  <th>Ottawa.ca Link</th>
   <th>Name</th>
+  <th>Link</th>
   </tr>
   </tr>
   <?php
@@ -73,16 +84,17 @@ function lobbyist_search($name) {
     ?>
     <tr>
     <td>
-    <a target="_blank" href="../<?php print $name; ?>/link">link</a>
+    <a href="../<?php print $name; ?>"><?php print $name; ?></a>
     </td>
     <td>
-    <a href="../<?php print $name; ?>"><?php print $name; ?></a>
+    <a target="_blank" href="../<?php print $name; ?>/link"><i class="icon-edit"></i></a>
     </td>
     </tr>
     <?php
   }
   ?>
   </table>
+  </span>
   </div>
   <?php
   bottom();
@@ -93,12 +105,12 @@ function lobbyist($name) {
   ?>
   <div class="row-fluid">
   <div class="span12">
-  <p><a target="_blank" href="<?php print $name; ?>/link">link</a></p>
+  <p><a target="_blank" href="<?php print $name; ?>/link"><i class="icon-edit"></i> direct link to ottawa.ca</a></p>
   </div>
   </div>
   <div class="row-fluid">
   <div class="span12">
-  <iframe style="width: 100%; height: 1200px;" src="<?php print $name; ?>/link"></iframe>
+  <iframe style="border: 0px; width: 100%; height: 1200px;" src="<?php print $name; ?>/link"></iframe>
   </div>
   </div>
   <?php
@@ -199,23 +211,33 @@ function top($title) {
 <link href="<?php print $OTT_WWW; ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" type="text/css">
 <style type="text/css">
   body {
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 20px;
 }
 </style>
 </head>
 <body>
-<div style="float: right;">
-<a href="<?php print $OTT_WWW; ?>">Home</a>
+<div style="background: #fcfcfc; padding: 10px; border: #c0c0c0 solid 1px;">
+
+<div class="row-fluid">
+<span class="span9">
+<div class="lead">
+<a href="<?php print $OTT_WWW; ?>"><img style="width: 50px; height: 50px;" src="<?php print $OTT_WWW; ?>/img/ottwatch.png"/></a>
+<?php print $title; ?>
 </div>
-<div>
-<div class="lead"><?php print $title; ?></div>
+</span>
+<span class="span3">
+<i>Created by <a href="http://kevino.ca"><b>Kevin O'Donnell</b></a> to make it easier to be part of the political conversation in Ottawa.</i>
+</span>
+</div>
+
 </div>
 <?php
 }
 
 function bottom() {
   ?>
+
+
   <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
   </body>
   </html>
