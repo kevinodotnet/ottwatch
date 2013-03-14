cdcf22e291f2ce7d1897423a55aeb5ab7bb8e8dd
Kevin O'Donnell <kevino@kevino.net>
2013-03-14 01:41:12 -0400

    api pretty printer/tester

---
+++ bin/api.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+A quick API tester. Pass in API url as first argument.
+Retrieves JSON and then pretty prints it.
+
+*/
+
+$d = file_get_contents($argv[1]);
+$c = json_decode($d);
+print print_r($c);
+
+?>
