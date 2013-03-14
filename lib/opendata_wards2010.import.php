c4a4273d6173eda7b6d69c03e34bc57b751de614
Kevin O'Donnell <kevino@kevino.net>
2013-03-13 23:59:24 -0400

    Wards projection fixed

--- lib/opendata_wards2010.import.php
+++ lib/opendata_wards2010.import.php
@@ -10,7 +10,7 @@ require_once('include.php');
 print `unzip -q opendata_wards2010.zip -d t`;
 
 $mysql = ' MYSQL:"'.OttWatchConfig::DB_NAME.',host='.OttWatchConfig::DB_HOST.',user='.OttWatchConfig::DB_USER.',password='.OttWatchConfig::DB_PASS.',port=3306" ';
-$cmd = " ogr2ogr -f 'MySQL' {$mysql} t/Wards_2010.shp -overwrite -lco SPATIAL_INDEX=NO ";
+$cmd = " ogr2ogr -t_srs 'EPSG:4326' -f 'MySQL' {$mysql} t/Wards_2010.shp -overwrite -lco SPATIAL_INDEX=NO ";
 
 print `$cmd`;
 
