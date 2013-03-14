6f1ad3f30e4c8e332541e8d6451635b69181cd1f
Kevin O'Donnell <kevino@kevino.net>
2013-03-13 23:38:23 -0400

    import is done

--- lib/opendata_wards2010.import.php
+++ lib/opendata_wards2010.import.php
@@ -6,15 +6,14 @@ $dirname = preg_replace("/\n/","",$dirname);
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 require_once('include.php');
 
-`rm -rf t`;
 `mkdir t`;
-print `unzip opendata_wards2010.zip -d t`;
+print `unzip -q opendata_wards2010.zip -d t`;
 
 $mysql = ' MYSQL:"'.OttWatchConfig::DB_NAME.',host='.OttWatchConfig::DB_HOST.',user='.OttWatchConfig::DB_USER.',password='.OttWatchConfig::DB_PASS.',port=3306" ';
-$cmd = " ogr2ogr -f 'MySQL' {$mysql} t/Wards_2010.shp ";
+$cmd = " ogr2ogr -f 'MySQL' {$mysql} t/Wards_2010.shp -overwrite -lco SPATIAL_INDEX=NO ";
 
-$cmd = " ogr2ogr -f 'KML' test.kml t/Wards_2010.shp ";
 print `$cmd`;
 
+`rm -rf t`;
 
 ?>
