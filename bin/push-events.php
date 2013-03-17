1cdba9f33399dec21dcf08d1b4e110649e97d113
Kevin O'Donnell <kevino@kevino.net>
2013-03-17 13:53:57 -0400

    Unified tweet and event controller conception

---
+++ bin/push-events.php
@@ -0,0 +1,11 @@
+<?php
+
+$dirname = `dirname $argv[0]`;
+$dirname = preg_replace("/\n/","",$dirname);
+
+set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
+require_once('include.php');
+
+EventController::pushNewEvents();
+
+?>
---
+++ www/controllers/EventController.php
@@ -0,0 +1,11 @@
+<?php
+
+class EventController {
+
+  public static function pushNewEvents() {
+    # tweet new lobbying entries
+  }
+
+}
+
+?>
