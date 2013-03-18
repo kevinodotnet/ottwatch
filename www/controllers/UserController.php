4153390709d6cd8d1ad7db469eefc23dd05ff130
Kevin O'Donnell <kevino@kevino.net>
2013-03-18 17:25:01 -0400

    Added map touser home screen

--- www/controllers/UserController.php
+++ www/controllers/UserController.php
@@ -38,7 +38,7 @@ class UserController {
     <?php 
     $rows = getDatabase()->all(" 
       select
-        p.rd_num, r.rd_name
+        p.id, p.rd_num, r.rd_name, r.rd_suffix
       from places p
         join roadways r on r.OGR_FID = p.roadid
       where personid = ".getSession()->get("user_id")."
@@ -72,8 +72,8 @@ class UserController {
     <input type="text" name="rd_name" class="input-small" placeholder="Street Name"/>
     <select name="rd_suff">
     <?php 
-    $rows = getDatabase()->all(" select distinct(rd_suffix) s from roadways where rd_suffix != 'NULL' order by rd_suffix ");
-    foreach ($rows as $r) {
+    $suffixes = getDatabase()->all(" select distinct(rd_suffix) s from roadways where rd_suffix != 'NULL' order by rd_suffix ");
+    foreach ($suffixes as $r) {
       ?>
       <option value="<?php print $r['s']; ?>"><?php print $r['s']; ?></option>
       <?php
@@ -83,6 +83,43 @@ class UserController {
     <button class="btn" onclick="saveNewPlace()">Add</button>
     </form>
     </div>
+    </div><!-- /span -->
+
+    <div class="span8">
+    <div id="map_canvas" style="width:100%; height:600px;"></div>
+    <script>
+        var mapOptions = { 
+          center: new google.maps.LatLng(45.420833,-75.69), 
+          zoom: 12, 
+          mapTypeId: google.maps.MapTypeId.ROADMAP 
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        <?php
+        foreach ($rows as $r) {
+          $address = "{$r['rd_num']} {$r['rd_name']} {$r['rd_suffix']}, Ottawa, Ontario";
+          ?>
+	        var contentString<?php print $r['id']; ?> = 
+            '<div>' + 
+            '<b><?php print $address; ?></b>' + 
+            '</div>';
+	        var infowindow<?php print $r['id']; ?> = new google.maps.InfoWindow({ content: contentString<?php print $r['id']; ?> });
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode( { 'address': '<?php print $address; ?>'}, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+		  	        var myLatlng<?php print $r['id']; ?> = new google.maps.LatLng(results[0].geometry.location.mb,results[0].geometry.location.nb);
+				        var marker<?php print $r['id']; ?> = new google.maps.Marker({ position: myLatlng<?php print $r['id']; ?>, map: map, title: '<?php print $address; ?>' }); 
+				        google.maps.event.addListener(marker<?php print $r['id']; ?>, 'click', function() {
+				          infowindow<?php print $r['id']; ?>.open(map,marker<?php print $r['id']; ?>);
+                });
+              }
+            });
+          <?php
+          print "/*\n";
+          pr($r);
+          print "*/\n";
+        }
+        ?>
+    </script>
     </div><!-- /span -->
 
     </div><!-- /row -->
