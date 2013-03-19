f8393435b302807349a03c066619dfb2a8aea037
Kevin O'Donnell <kevino@kevino.net>
2013-03-19 16:43:40 -0400

    Poll map viewing and linkage to other API elements

--- www/controllers/ApiController.php
+++ www/controllers/ApiController.php
@@ -17,6 +17,21 @@ class ApiController {
     return $result;
   }
 
+  public static function getPolygonAsArray($text) {
+    # LINESTRING(-75.74452847405216 45.38879029192849, ..., -75.74384484749845 45.389000882100305)
+    $text = preg_replace('/POLYGON\(\(/',"",$text);
+    $text = preg_replace('/\)\)/',"",$text);
+    $points = array();
+    foreach (explode(",",$text) as $p) {
+      $t = explode(" ",$p);
+      $np = array();
+      $np['lat'] = $t[1];
+      $np['lon'] = $t[0];
+      $points[] = $np;
+    }
+    return $points;
+  }
+
   public static function getLinestringAsArray($text) {
     # LINESTRING(-75.74452847405216 45.38879029192849, ..., -75.74384484749845 45.389000882100305)
     $text = preg_replace('/LINESTRING\(/',"",$text);
@@ -101,12 +116,13 @@ class ApiController {
     foreach ($row as $k => $v) {
       $result[$k] = $v;
     }
-    $midpoint =  self::getLatLonFromPoint($row['midpoint']);
+    $midpoint = self::getLatLonFromPoint($row['midpoint']);
     unset($result['midpoint']); # = $midpoint;
     $points = self::getLinestringAsArray($row['points']);
     $result['points'] = $points;
     $t = self::point($midpoint['lat'],$midpoint['lon']);
     $result['ward'] = $t['ward'];
+    $result['polls'] = $t['polls'];
 
     return $result;
   }
@@ -135,11 +151,138 @@ class ApiController {
     $row = getDatabase()->one(" select ward_num,ward_en,ward_fr from wards_2010 where ST_Contains(shape,PointFromText('POINT($lon $lat)')) ");
     if ($row['ward_num']) {
       $result['ward'] = getApi()->invoke('/api/wards/'.$row['ward_num']);
+
+	    # also get the poll data
+	    $row = getDatabase()->one(" select vot_subd from polls_2010 where ST_Contains(shape,PointFromText('POINT($lon $lat)')) ");
+      $polls = array();
+      if ($row['vot_subd']) {
+        $polls['2010'] = $row['vot_subd'];
+      }
+      $result['polls'] = $polls;
+
     } else {
       $result['ward'] = self::errResult("Point is not within Ottawa city limits");
     }
 
     return $result;
+  }
+
+  public static function listWards() {
+    $rows = getDatabase()->all(" select * from wards_2010 ");
+    $result = array();
+    foreach ($rows as $r) {
+      $result[$r['ward_num']] = $r['ward_en'];
+    }
+    return $result;
+  }
+
+  public static function wardPollMapStatic($wardnum,$year,$pollnum) {
+    $poll = self::wardPoll($wardnum,$year,$pollnum);
+    if (!$poll['ward']) {
+      print "Not found\n";
+      return;
+    }
+
+    $url = "http://maps.googleapis.com/maps/api/staticmap";
+    $url .= "?size=640x640";
+    $url .= "&scale=2";
+    $url .= "&maptype=roadmap";
+    $url .= "&sensor=false";
+    $url .= "&path=color:0xff000044|weight:5|fillcolor:0xFF000044"; # |8th+Avenue+%26+34th+St,New+York,NY|\8th+Avenue+%26+42nd+St,New+York,NY|Park+Ave+%26+42nd+St,New+York,NY,NY|\Park+Ave+%26+34th+St,New+York,NY,NY";
+    foreach ($poll['polygon'] as $p) {
+      $url .= "|{$p['lat']},{$p['lon']}";
+    }
+    print "<img src=\"$url\"/>";
+  }
+
+  public static function wardPollMapLive($wardnum,$year,$pollnum) {
+    $poll = self::wardPoll($wardnum,$year,$pollnum);
+    if (!$poll['ward']) {
+      print "Not found\n";
+      return;
+    }
+    ?>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php print OttWatchConfig::GOOGLE_API_KEY; ?>&sensor=false"></script>
+    <div id="map_canvas" style="width:100%; height:100%;"></div>
+    <script>
+
+google.maps.Polygon.prototype.getBounds = function() {
+    var bounds = new google.maps.LatLngBounds();
+    var paths = this.getPaths();
+    var path;        
+    for (var i = 0; i < paths.getLength(); i++) {
+        path = paths.getAt(i);
+        for (var ii = 0; ii < path.getLength(); ii++) {
+            bounds.extend(path.getAt(ii));
+        }
+    }
+    return bounds;
+}
+
+        var mapOptions = { 
+          center: new google.maps.LatLng(45.420833,-75.69), 
+          zoom: 10, 
+          mapTypeId: google.maps.MapTypeId.ROADMAP 
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        var coords = [
+        <?php
+        $center = $poll['center'];
+        foreach ($poll['polygon'] as $p) {
+          print "new google.maps.LatLng({$p['lat']},{$p['lon']}),\n";
+        }
+        ?>
+        ];
+
+			  pollPoly = new google.maps.Polygon({
+			    paths: coords,
+			    strokeColor: "#FF0000",
+			    strokeOpacity: 0.8,
+			    strokeWeight: 2,
+			    fillColor: "#FF0000",
+			    fillOpacity: 0.35
+			  });
+			  pollPoly.setMap(map);
+
+        var centerLatlng = new google.maps.LatLng(<?php print $center['lat']; ?>,<?php print $center['lon']; ?>);
+        map.panTo(centerLatlng);
+        map.fitBounds(pollPoly.getBounds());
+
+    </script>
+
+    <?php
+  }
+
+  public static function wardPoll($wardnum,$year,$pollnum) {
+    $table = '';
+    if ($year == '2010') {
+      $table = 'polls_2010';
+    } else {
+      $result = array();
+      $result['error'] = "unknown election year: $year";
+      return $result;
+    }
+    $row = getDatabase()->one(" select OGR_FID,astext(shape) polygon,astext(centroid(shape)) center from $table where ward = :wardnum and vot_subd = :vot_subd ",array("wardnum"=>$wardnum,'vot_subd'=>$pollnum));
+    if (!$row['OGR_FID']) {
+      $result = array();
+      $result['error'] = "poll not found";
+      return $result;
+    }
+    $result = array();
+    $result['ward'] = $wardnum;
+    $result['year'] = $year;
+    $result['pollnum'] = $pollnum;
+    $result['polygon'] = self::getPolygonAsArray($row['polygon']);
+    $result['center'] = self::getLatLonFromPoint($row['center']);
+    return $result;
+  }
+
+  public static function wardPolls($wardnum) {
+    $rows = getDatabase()->all(" select vot_subd from polls_2010 where ward = :wardnum ",array("wardnum"=>$wardnum));
+    $polls = array();
+    foreach ($rows as $r) { $polls[] = $r['vot_subd']; }
+    $result['2010'] = $polls;
+    pr($result);
   }
 
   public static function ward($wardnum) {
@@ -187,3 +330,6 @@ class ApiController {
   }
 
 }
+
+
+
