e6ae69bc5e3910fa9db8d1072d4c9b9623e8f864
Kevin O'Donnell <kevino@kevino.net>
2013-05-13 09:18:06 -0400

    Removed Login from ui because it is not implemented and probably wont be

--- www/index.php
+++ www/index.php
@@ -355,14 +355,18 @@ function copyToClipboard (text) {
 <li><a href="<?php print $OTT_WWW; ?>/api/about">API</a></li>
 <?php
 if (!LoginController::isLoggedIn()) {
+  /*
   ?>
   <li><a href="<?php print $OTT_WWW; ?>/user/login">Login</a></li>
   <?php
+  */
 } else {
+  /*
   ?>
   <li><a href="<?php print $OTT_WWW; ?>/user/home"><?php print getSession()->get('user_email'); ?></a></li>
   <li><a href="<?php print $OTT_WWW; ?>/user/logout">Logout</a></li>
   <?php
+  */
 }
 ?>
 </ul>
