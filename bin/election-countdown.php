b4185557190f1d219a79305a7aef93f04a6bf909
Kevin O'Donnell <kevino@kevino.net>
2013-07-29 18:57:50 -0400

    election countdown

---
+++ bin/election-countdown.php
@@ -0,0 +1,20 @@
+<?php
+
+$dirname = `dirname $argv[0]`;
+$dirname = preg_replace("/\n/","",$dirname);
+
+set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
+require_once('include.php');
+require_once('twitteroauth.php');
+
+$datetime1 = date_create('2014-10-27');
+$datetime2 = date_create('now');
+$diff = date_diff($datetime1,$datetime2);
+$days = $diff->days;
+
+if ($days > 0 && $days % 25 == 0) {
+  print("$days until election day\n");
+  # tweet("$days until election day",1);
+}
+
+?>
