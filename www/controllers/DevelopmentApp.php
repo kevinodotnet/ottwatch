d9ce524a8d3dbe78d33e162b1b1f32a411cd456b
Kevin O'Donnell <kevino@kevino.net>
2013-07-29 19:32:51 -0400

    better ordering of devapp docs

--- www/controllers/DevelopmentApp.php
+++ www/controllers/DevelopmentApp.php
@@ -71,7 +71,7 @@ class DevelopmentAppController {
     ?>
     <tr><td>Documents</td><td>
     <?php
-    $docs = getDatabase()->all(" select * from devappfile where devappid = :id order by title ",array('id'=>$a['id']));
+    $docs = getDatabase()->all(" select * from devappfile where devappid = :id order by updated desc,title ",array('id'=>$a['id']));
     foreach ($docs as $d) {
       $doctitle = $d['title'];
       $doctitle = preg_replace("/{$a['devid']} /","",$doctitle);
@@ -550,7 +550,12 @@ class DevelopmentAppController {
     }
 
     getDatabase()->execute(" delete from devappfile where devappid = :devappid ",array( 'devappid' => $id,));
+		$touched = array();
     foreach ($files as $f) {
+			if (isset($touched[$f['href']])) {
+				continue;
+			}
+			$touched[$f['href']] = 1;
       getDatabase()->execute(" insert into devappfile (devappid,href,title,created,updated) values (:devappid,:href,:title,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP) ",array(
         'devappid' => $id,
         'href' => $f['href'],
