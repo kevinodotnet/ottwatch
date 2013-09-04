ff8a3f034559ed36114c19d215f71e93e5f05fa9
Kevin O'Donnell <kevino@kevino.net>
2013-09-03 23:22:28 -0400

    crawling improvements

--- www/controllers/ConsultationController.php
+++ www/controllers/ConsultationController.php
@@ -18,7 +18,7 @@ class ConsultationController {
       $matches[2][$x] = preg_replace('/Learn more about /','',$matches[2][$x]);
       $url = $matches[1][$x];
       $category = $matches[2][$x];
-      self::crawlCategory($category,$url);
+      self::crawlCategory($category,"http://ottawa.ca{$url}");
       #exit;
     }
   }
@@ -27,19 +27,30 @@ class ConsultationController {
 
   public static function crawlCategory ($category, $url) {
     #print "CATEGORY: $category, URL: $url\n";
-    $html = file_get_contents("http://ottawa.ca{$url}");
-    $html = preg_replace("/\n/"," ",$html);
-    $html = strip_tags($html,"<a>");
-    preg_match_all("/<a[^h]+href=\"\/en\/city-hall([^\"]+public-consultations[^\"]+)\"[^>]*>([^<]*)<\/a>/",$html,$matches);
-    #print_r($matches);
-    for ($x = 0; $x < count($matches[0]); $x++) {
-      $url = $matches[1][$x];
-      $url = "http://ottawa.ca/en/city-hall{$url}";
-      $title = $matches[2][$x];
-      #$title = utf8_decode($title);
-      $title = html_entity_decode($title,ENT_QUOTES);
+    $html = file_get_contents($url);
+
+    $html = preg_replace("/\n/","KEVINO_NEWLINE",$html);
+    $html = preg_replace("/<head.*<body/","<body",$html);
+    $html = preg_replace("/<script[^<]+<\/script>/"," ",$html);
+    $html = preg_replace("/KEVINO_NEWLINE/","\n",$html);
+    $html = preg_replace("/ & /"," and ",$html);
+
+    $html = strip_tags($html,"<div><a>");
+    $xml = simplexml_load_string($html);
+    $div = $xml->xpath('//div[@id="cityott-content"]');
+    $div = simplexml_load_string($div[0]->asXML());
+
+    $links = $div->xpath('//a');
+    foreach ($links as $a) {
+      $url = $a->attributes();
+      if (substr($url,0,1) == '/') {
+        $url = "http://ottawa.ca{$url}";
+      }
+
+      $title = $a[0];
       self::crawlConsultation($category,$title,$url);
     }
+
   }
 
   // crawl a specific consultation 
@@ -58,14 +69,18 @@ class ConsultationController {
     $html = preg_replace("/<script[^<]+<\/script>/"," ",$html);
     $html = preg_replace("/KEVINO_NEWLINE/","\n",$html);
     $html = preg_replace("/ & /"," and ",$html);
-    $html = strip_tags($html,"<div><a><section>");
-    # $lines = explode("\n",$html); for ($x = 0; $x < count($lines); $x++) { print "[$x]: {$lines[$x]}\n"; }
+    $html = strip_tags($html,"<div><a>");
+
+    # the view-dom-id CLASS changes randomly, so remove it
+    # example: view-dom-id-b477d62d0bdb286acc260d50c820060d
+    $html = preg_replace("/view-dom-id-[a-z0-9]+/","",$html);
+
     $xml = simplexml_load_string($html);
-    $section = $xml->xpath('//section[@id="main-content"]');
-    $section = simplexml_load_string($section[0]->asXML());
     $div = $xml->xpath('//div[@id="cityott-content"]');
     $contentMD5 = md5($div[0]->asXML());
     print "MD5: $contentMD5\n";
+    file_put_contents($contentMD5,$div[0]->asXML());
+
     $div = simplexml_load_string($div[0]->asXML());
     $links = $div->xpath('//a');
     foreach ($links as $a) {
@@ -73,11 +88,13 @@ class ConsultationController {
       $docLink = $a->attributes();
       $docTitle = $a[0];
       print "  DOCUMENT: $docTitle $docLink\n";
+      print implode("|",array("CSV",$category,$title,$url,$docTitle,$docLink,"\n"));
     }
   }
   
 }
 
 ConsultationController::crawlConsultations();
+#ConsultationController::crawlCategory("TEST","http://ottawa.ca/en/city-hall/public-consultations/transportation");
 
 ?>
