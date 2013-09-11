96d2451ffcc27fde3415730ccfc064175c2913a6
Kevin O'Donnell <kevino@kevino.net>
2013-09-11 10:24:34 -0400

    fix twitter include; lobby graph first commit

--- bin/consultations-tweeter.php
+++ bin/consultations-tweeter.php
@@ -6,6 +6,7 @@ $dirname = preg_replace("/\n/","",$dirname);
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../www");
 require_once('include.php');
+require_once('twitteroauth.php');
 
 ConsultationController::tweetUpdatedConsultations();
 
