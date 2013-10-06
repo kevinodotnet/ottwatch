c99c58aee434e02562bf297add64ad1c083244f9
Kevin O'Donnell <kevino@kevino.net>
2013-10-05 20:07:47 -0400

    bah; bad drunk coder

--- www/controllers/MeetingController.php
+++ www/controllers/MeetingController.php
@@ -963,10 +963,8 @@ class MeetingController {
       select m.id,m.category,id,meetid,date(starttime) starttime
       from meeting m 
         left join category c on c.category = m.category 
-      where
-        datediff(starttime,CURRENT_TIMESTAMP) < 120)
       ".
-      ($category == '' ? '' : ' and c.category = :category ')
+      ($category == '' ? '' : ' where c.category = :category ')
       ."
       order by starttime desc ",
       array('category' => $category));
