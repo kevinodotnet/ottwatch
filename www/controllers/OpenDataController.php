24c1c00c2cf30a99e4fa46569822e756273daead
Kevin O'Donnell <kevino@kevino.net>
2013-10-28 13:27:50 -0400

    Opendata UI listing

--- www/controllers/OpenDataController.php
+++ www/controllers/OpenDataController.php
@@ -59,6 +59,70 @@ class OpenDataController {
 	  $dataset = self::callData("/api/1/rest/dataset/$name");
 	  return $dataset;
 	}
+
+  /*
+
+  */
+  public static function doList() {
+    top();
+
+    ?>
+    <div class="row-fluid">
+    <div class="span3">
+    <h1>OpenData</h1>
+    </div>
+    <div class="span9">
+    <p class="lead">
+    <b>data.ottawa.ca</b> is Ottawa's open data portal. As data is added/updated
+    on the portal it will pop to the top of the below list. For more information about
+    each file use the <i>dataset</i> link to learn who maintains the data, and metadata
+    about the file itself.
+    </p>
+    </div>
+    </div>
+    <?php
+
+    $rows = getDatabase()->all("
+      select
+        d.title,d.url,
+        f.size,f.description,f.format,f.name as fname,f.url as fileurl, f.updated
+      from opendatafile f
+        join opendata d on d.id = f.dataid
+      order by
+        f.updated desc
+    ");
+    ?>
+    <table class="table table-bordered table-hover table-condensed" style="width: 100%;">
+    <tr>
+    <th>Updated</th>
+    <th>File</th>
+    <th>Dataset</th>
+    <th>Size (kB)</th>
+    <th>Format</th>
+    <th>Description</th>
+    </tr>
+    <?php
+    foreach ($rows as $r) {
+      $size = $r['size'];
+      if ($size > 0) {
+        $size = intval($size/1024);
+      }
+      ?>
+      <tr>
+      <td><?php print $r['updated']; ?></td>
+      <td><a href="<?php print $r['fileurl']; ?>"><?php print $r['fname']; ?></a></td>
+      <td><a href="<?php print $r['url']; ?>"><?php print $r['title']; ?></a></td>
+      <td><?php print $size ?></td>
+      <td><?php print $r['format']; ?></td>
+      <td><?php print $r['description']; ?></td>
+      </tr>
+      <?php
+    }
+    ?>
+    </table>
+    <?php
+    bottom();
+  }
 	
 }
 
