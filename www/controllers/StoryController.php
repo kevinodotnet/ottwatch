761a6648e2ae23cdc7f2ec68c39c04879094a0f7
Kevin O'Donnell <kevino@kevino.net>
2013-10-28 22:17:29 -0400

    story editing and preview coming together

--- www/controllers/StoryController.php
+++ www/controllers/StoryController.php
@@ -24,31 +24,59 @@ class StoryController {
     <div class="row-fluid">
     <div class="span6">
 
+    <script>
+    function preview() {
+      html = $('#storybody').val();
+      $('#previewbody').html(html);
+      return false;
+    }
+    </script>
+		<script src="<?php print OttWatchConfig::WWW; ?>/vendor/ckeditor/ckeditor.js"></script>
+		<script src="<?php print OttWatchConfig::WWW; ?>/vendor/ckeditor/adapters/jquery.js"></script>
+
+    <!--
     Created: <?php print $story['created']; ?><br/>
     Updated: <?php print $story['updated']; ?><br/>
+    -->
     <form class="form-horizontal" method="post" action="../save">
     <input type="hidden" name="id" value="<?php print $id; ?>"/>
+
+    <p>
+    <center>
+    <input style="width: 98%;" type="text" name="title" value="<?php print $story['title']; ?>"/>
+    </center>
+    </p>
+    <p>
+    <center>
     <button type="submit" name="save" class="btn">Save</button>
     <button type="submit" name="publish" value="1" class="btn">Publish</button>
     <button type="submit" name="unpublish" value="1" class="btn">Unpublish</button>
-    <button type="submit" name="delete" value="1" class="btn">Delete</button><br/><br/>
-    Title<br/>
-    <input style="width: 60%;" type="text" name="title" value="<?php print $story['title']; ?>"/>
-    <br/>
-    <br/>
-    Body<br/>
-    <textarea name="body" style="width: 80%; height: 400px;"><?php print $story['body']; ?></textarea>
+    <button type="submit" name="delete" value="1" class="btn">Delete</button>
+    <button type="submit" onclick="return preview()" name="delete" value="1" class="btn">Preview</button>
+    </center>
+    </p>
+    <textarea id="storybody" name="body" rows="80" style=""><?php print $story['body']; ?></textarea>
+    <script>
+    $( '#storybody' ).ckeditor({
+     toolbar: [ 
+	      ['Source','Maximize'],
+	      ['Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ],
+        [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        [ 'Image','Link','Unlink','Anchor','HorizontalRule' ],
+        [ 'Format','Font','FontSize' ],
+      ],
+      height: '450'
+    });
+    </script>
     </form>
 
     </div>
 
     <div class="span6">
-    <?php
-    print "<h1>{$story['title']}</h1>\n";
-    print $story['body'];
-    ?>
+    <h1 id="previewtitle"><?php print "{$story['title']}\n"; ?></h1>
+    <div id="previewbody"><?php print $story['body']; ?></div>
     </div>
-    </div>
+    </div><!-- /row -->
 
     <?php
 
