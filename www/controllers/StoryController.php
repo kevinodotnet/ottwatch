7e43d3c04d1c68278c0ea5e12e8fc4ea489ea345
Kevin O'Donnell <kevino@kevino.net>
2013-10-28 22:43:49 -0400

    public view of published stories

--- www/controllers/StoryController.php
+++ www/controllers/StoryController.php
@@ -11,6 +11,66 @@ class StoryController {
     header("Location: edit/$id");
   }
 
+  public static function show($id,$restTitle) {
+    $story = getDatabase()->one(" 
+      select 
+        s.title,s.body,
+        p.name as author,
+        left(s.updated,16) updated,
+        s.published,s.deleted,
+        p.*
+      from story s
+        join people p on p.id = s.personid
+      where 
+        s.id = :id 
+      ",array('id'=>$id));
+
+    # disallow linking to /story/X/WHATEVER_SOMEONE_TYPED by redirecting if the supplied
+    # "url_title" does not match the actual story title. Also helps if story title was 
+    # changed but links are already out there on social media
+    # title - urlencode - removed junk - lowercase
+    $titleToUrlPart = $story['title'];
+    $titleToUrlPart = preg_replace('/ /','-',$titleToUrlPart);
+    $titleToUrlPart = urlencode($titleToUrlPart);
+    $titleToUrlPart = preg_replace('/%../','',$titleToUrlPart);
+    $titleToUrlPart = strtolower($titleToUrlPart);
+    if ($titleToUrlPart != $restTitle) {
+      # new title, or someone is having fun
+      $url = OttWatchConfig::WWW."/story/{$id}/{$titleToUrlPart}";
+      header("Location: $url");
+      return;
+    }
+
+    top($story['title']);
+    if ($story['deleted'] == 1) {
+      ?>
+      <h1>Error: this story has been deleted</h1>
+      <?php
+      bottom();
+      return;
+    }
+    if ($story['published'] == 0) {
+      ?>
+      <h1>Error: this story is not yet published</h1>
+      <?php
+      bottom();
+      return;
+    }
+    ?>
+    <div class="row-fluid">
+    <div class="offset4 span4">
+    <h1 id="previewtitle"><?php print "{$story['title']}\n"; ?></h1>
+    <p style="float: right; text-align: right;">
+    <b><?php print $story['author']; ?></b><br/>
+    <?php print $story['updated']; ?>
+    </p>
+    <p style="border-top: 1px solid #f0f0f0; clear: both;"><?php print $story['body']; ?></p>
+    </div>
+    </div><!-- /row -->
+    <?php
+    bottom();
+  }
+
   public static function edit($id) {
     top();
     $story = getDatabase()->one(" select * from story where id = :id ",array('id'=>$id));
