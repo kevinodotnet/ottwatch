c7aa81cfec1a47ecfa6e65bd2721e70b6f4dc19b
Kevin O'Donnell <kevino@kevino.net>
2014-01-03 10:01:40 -0500

    stoopid

--- bin/page-monitor.php
+++ bin/page-monitor.php
@@ -6,7 +6,7 @@ $dirname = preg_replace("/\n/","",$dirname);
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../www");
 require_once('include.php');
-require_once('../vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 /* Monitory an ottawa.ca drupal page for md5 hash changes. */
 
