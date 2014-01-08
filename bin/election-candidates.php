1e534d6910e84dc13467649f3b2dab1fe259b033
Kevin O'Donnell <kevino@kevino.net>
2014-01-07 19:35:52 -0500

    trustee hacking

--- bin/election-candidates.php
+++ bin/election-candidates.php
@@ -7,6 +7,75 @@ set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../www");
 require_once('include.php');
 
+##########################################################################################
+# TRUSTEE
+##########################################################################################
+
+$url = "http://ottawa.ca/en/city-hall/your-city-government/elections/school-board-trustee";
+
+$html = file_get_contents($url);
+$html = ConsultationController::getCityContent($html,"<h3><table><tr><td><th>");
+
+$html = preg_replace("/\n/",'',$html);
+$html = preg_replace("/<tr/","\n<tr",$html);
+$html = preg_replace("/<\/tr>/","<\/tr>\n",$html);
+$html = preg_replace("/<h2/","\n<h2",$html);
+$html = preg_replace("/<\/h2>/","<\/h2>\n",$html);
+$html = preg_replace("/<h3/","\n<h3",$html);
+$html = preg_replace("/<\/h3>/","<\/h3>\n",$html);
+$lines = explode("\n",$html);
+
+foreach ($lines as $l) {
+
+	if (preg_match('/<h2/',$l)) {
+		$board = strip_tags($l);
+		continue;
+	}
+	if (preg_match('/<h3>/',$l)) {
+		$matches = array();
+		if (!preg_match('/<h3>Zone (\d+)/',$l,$matches)) {
+			print "ERROR: $l\n";
+		}
+		$zone = $matches[1];
+		continue;
+	}
+
+	if (!preg_match('/^<tr/',$l)) { continue; }
+	if (preg_match('/Email Address/',$l)) { continue; }
+	if (preg_match('/No candidate/i',$l)) { continue; }
+  $l = preg_replace('/<td>/',"\t",$l);
+  $l = preg_replace('/not provided/',"",$l);
+  $l = preg_replace('/not provided/',"",$l);
+  $l = strip_tags($l);
+  $l = trim($l);
+  $row = explode("\t",$l);
+
+  $names = explode(" ",$row[0]);
+
+  $candidate['ward'] = $zone;
+  $candidate['wardname'] = $board;
+  $candidate['first'] = trim($names[0]);
+  $candidate['last'] = trim($names[count($names)-1]);
+  $candidate['phone'] = trim(@$row[1]);
+  $candidate['fax'] = trim(@$row[2]);
+  $candidate['email'] = trim(@$row[3]);
+
+  $c = " select count(1) c from candidate where ward = ${candidate['ward']} and first = '{$candidate['first']}' and last = '{$candidate['last']}'; ";
+  $i = "insert into candidate (ward,year,first,last) values ({$candidate['ward']},2014,'{$candidate['first']}','{$candidate['last']}'); ";
+  $u = "update candidate set nominated = (case when nominated is null then now() else nominated end) , phone = '{$candidate['phone']}', email = '{$candidate['email']}'  where ward = {$candidate['ward']} and first = '{$candidate['first']}' and last = '{$candidate['last']}'; ";
+  $key = "{$candidate['ward']} {$candidate['first']} {$candidate['last']}";
+  $sql[] = array('ward'=>$candidate['ward'],'insert'=>$i,'update'=>$u,'count'=>$c,'details'=>$candidate);
+
+}
+
+pr($sql);
+
+return;
+
+##########################################################################################
+# MAYOR
+##########################################################################################
+
 $url = "http://ottawa.ca/en/city-hall/your-city-government/elections/mayor";
 $html = file_get_contents($url);
 $html = ConsultationController::getCityContent($html,"<h3><table><tr><td><th>");
@@ -48,6 +117,10 @@ foreach ($lines as $l) {
   $key = "$ward {$candidate['first']} {$candidate['last']}";
   $sql[] = array('ward'=>$ward,'name'=>$key,'insert'=>$i,'update'=>$u,'count'=>$c,'details'=>$candidate);
 }
+
+##########################################################################################
+# CANDIDATES
+##########################################################################################
 
 $url = "http://ottawa.ca/en/city-hall/your-city-government/elections/councillor";
 $html = file_get_contents($url);
