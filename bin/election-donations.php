eae28fc5d4707fc8de7c81e70c11a89e42682f09
Kevin O'Donnell <kevino@kevino.net>
2014-01-12 17:06:28 -0500

    donation/geo matching logic improvements

--- bin/election-donations.php
+++ bin/election-donations.php
@@ -7,72 +7,121 @@ set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../www");
 require_once('include.php');
 
+if (@$argv[1] == 'setinclude') {
+	$code = file_get_contents('set.include');
+	eval($code);
+	return;
+}
+
 $total = 0;
 $matched = 0;
 
-	$rows = getDatabase()->all(" select * from candidate_donation where location is null ");
-	foreach ($rows as $row) {
-		$total ++;
-		if (setDonorLocation($row)) {
-			$matched ++;
-		}
-		print "($matched/$total/".count($rows).") {$row['id']} {$row['address']} - {$row['postal']} - {$row['name']} - {$row['amount']}\n";
+$rows = getDatabase()->all(" select * from candidate_donation where city = 'Ottawa' and location is null ");
+foreach ($rows as $row) {
+	$total ++;
+	if (setDonorLocation($row)) {
+		$matched ++;
 	}
+	print "PROGRESS ($matched/$total/".count($rows).")\n";
+}
 
 function setDonorLocation($row) {
+
+	print "id: {$row['id']} ADDRESS: {$row['address']} POSTAL: {$row['postal']} NAME: {$row['name']}\n";
 	
 	$debug = 0;
-
 	$postal = $row['postal'];
 	$addr = strtoupper($row['address']);
-
-	if ($debug) { print ">>> $addr >>> $postal <<<\n"; }
 
 	# group by because some addresses have multiple property parcels.
 	$sql = "
 		select 
-			min(ottwatchid) ottwatchid,ADDRESS_NUMBER, ROAD_NAME , POSTAL_CODE
+			min(ottwatchid) ottwatchid,ADDRESS_NUMBER, ROAD_NAME, POSTAL_CODE
 		from geo_property 
 		where 
 			postal_code = '$postal' 
 		group by ROAD_NAME, ADDRESS_NUMBER, POSTAL_CODE
-		order by ROAD_NAME, ADDRESS_NUMBER, POSTAL_CODE
+		order by address_number, ROAD_NAME, ADDRESS_NUMBER, POSTAL_CODE
 	";
 	$props = getDatabase()->all($sql);
 	$prop_matches = filterPropertyMatches($row,$props);
+
+	if (count($prop_matches) == 0) {
+		# no short cut based on postal, so fall back to checking permutations of address parts
+		# against ADDRESS_NUMBER and ROAD_NAME. Bruteforce-sih, but workable.
+		$regaddr = $addr;
+		$regaddr = preg_replace('/,/',' ',$regaddr);
+		$regaddr = preg_replace('/-/',' ',$regaddr);
+		$regaddr = preg_replace('/#/',' ',$regaddr);
+		$regaddr = preg_replace('/\./',' ',$regaddr);
+		$regaddr = preg_replace('/  /',' ',$regaddr);
+		$parts = explode(' ',$regaddr);
+		foreach ($parts as $num) {
+			if (!preg_match('/^\d+$/',$num)) { continue; }
+			foreach ($parts as $road) {
+				if ($num != $road) {
+					$road = preg_replace("/'/","''",$road);
+					$sql = "
+						select 
+							min(ottwatchid) ottwatchid,ADDRESS_NUMBER, ROAD_NAME , POSTAL_CODE
+						from geo_property 
+						where 
+							ADDRESS_NUMBER = $num
+							and ROAD_NAME = '$road'
+						group by ROAD_NAME, ADDRESS_NUMBER, POSTAL_CODE
+						order by address_number, ROAD_NAME, ADDRESS_NUMBER, POSTAL_CODE
+					";
+					$moreros = getDatabase()->all($sql);
+					foreach ($moreros as $r) {
+						$props[] = $r;
+					}
+				}
+			}
+		}
+		$prop_matches = filterPropertyMatches($row,$props);
+	}
+
 	if (count($prop_matches) == 1) {
 		# best case yo!
-		$geo = getDatabase()->one(" select astext(centroid(shape)) centroid from geo_property where ottwatchid = {$prop_matches[0]['ottwatchid']} ");
+		setDonationLocation($row['id'],$prop_matches[0]['ottwatchid']);
+		return true;
+	} else {
+		foreach ($prop_matches as $p) {
+			print "  pickmatch: ottwatchid: {$p['ottwatchid']} num: {$p['ADDRESS_NUMBER']} roadname: {$p['ROAD_NAME']} postal: {$p['POSTAL_CODE']}\n";
+			print "    setDonationLocation({$row['id']},{$p['ottwatchid']}); \n";
+		}
+	}
+
+	return false;
+}
+
+function setDonationLocation($donationid,$propertyottwatchid) {
+		$geo = getDatabase()->one(" select astext(centroid(shape)) centroid from geo_property where ottwatchid = $propertyottwatchid ");
 		$mercator = getLatLonFromPoint($geo['centroid']);
 		$latlon = mercatorToLatLon($mercator['lon'],$mercator['lat']);
 		$lat = $latlon['lat'];
 		$lon = $latlon['lon'];
 		$pointval = "PointFromText('POINT($lon $lat)')";
 		// $sql = " update candidate_donation set location = ( select centroid(shape) from geo_property where ottwatchid = {$prop_matches[0]['ottwatchid']} ) where id = {$row['id']} ";
-		$sql = " update candidate_donation set location = $pointval where id = {$row['id']} ";
+		$sql = " update candidate_donation set location = $pointval where id = $donationid ";
 		getDatabase()->execute($sql);
-		return true;
-	} else {
-		if ($debug) { print "NO JOY: ".count($prop_matches)." matches\n"; }
-	}
-
-	return false;
 }
 
 function filterPropertyMatches($row,$props) {
-	$debug = 0;
+	$debug = 1;
 	$postal = $row['postal'];
 	$addr = strtoupper($row['address']);
 	$prop_matches = array();
 	foreach ($props as $p) {
-		if ($debug) { pr($p); }
 		$p['ROAD_NAME'] = trim($p['ROAD_NAME']);
-		if ($debug) { print " >>> (match: $postal) >>> {$p['ADDRESS_NUMBER']} >>> {$p['ROAD_NAME']} >>> {$p['POSTAL_CODE']} >>> "; }
+		if ($debug) { print " checking: num: {$p['ADDRESS_NUMBER']} roadname: {$p['ROAD_NAME']} postal: {$p['POSTAL_CODE']} >>> "; }
 		if (preg_match("/.*{$p['ADDRESS_NUMBER']}.*/",$addr)) {
-			if ($debug) { print "ADDRESS_NUMBER_MATCH "; }
+			if (preg_match("/[0-9]{$p['ADDRESS_NUMBER']}/",$addr)) { if ($debug) { print "\n"; } continue; } //
+			if (preg_match("/{$p['ADDRESS_NUMBER']}[0-9]/",$addr)) { if ($debug) { print "\n"; } continue; } //
+			if ($debug) { print "addrMatch "; }
 			# address of property is somewhere in donor address!
 			if (preg_match("/.*{$p['ROAD_NAME']}.*/",$addr)) {
-				if ($debug) { print " ROAD_NAME_MATCH "; }
+				if ($debug) { print " roadNameMatch "; }
 				# name of road is in there too! best case for a match!
 				$prop_matches[] = $p;
 			}
