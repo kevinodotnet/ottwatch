6fde6555355dadd0bb884509341d6148a207a2b6
Kevin O'Donnell <kevino@kevino.net>
2014-03-31 11:09:45 -0400

    better debug and change tracking of public notifications

--- bin/public-notice-tweeter.php
+++ bin/public-notice-tweeter.php
@@ -20,7 +20,6 @@ $url = "http://ottawa.ca/en/city-hall/accountability-and-transparency/public-mee
 $html = file_get_contents($url);
 $html = ConsultationController::getCityContent($html,'');
 $md5 = md5($html);
-
 $prevMD5 = getvar('public-meetings-and-notices.md5');
 
 if ($md5 == $prevMD5) {
@@ -28,12 +27,17 @@ if ($md5 == $prevMD5) {
   return;
 }
 
+# save for delta research later
+file_put_contents(OttWatchConfig::FILE_DIR.'/public_notices_'.time().'_'.$md5.'.html',$html);
 setvar('public-meetings-and-notices.md5',$md5);
 
-# tweet that public notices page has updated
+# help a brother out
+print "\n\n";
+print "diff ".OttWatchConfig::FILE_DIR.'/public_notices*'.$prevMD5.'* '.OttWatchConfig::FILE_DIR.'/public_notices*'.$md5."*\n";
+print "\n\n";
 
+# tweet that public notices page has updated
 $tweet = tweet_txt_and_url("Public Notices & Meetings updated",$url);
 tweet($tweet);
-
 
 ?>
