9482fd34e508203d60f2dc13941abba2c9386431
Kevin O'Donnell <kevino@kevino.net>
2014-05-09 10:54:40 -0400

    fixed the latelobby errors IHOPE

--- bin/lobby-scaper.php
+++ bin/lobby-scaper.php
@@ -7,7 +7,17 @@ $dirname = preg_replace("/\n/","",$dirname);
 set_include_path(get_include_path() . PATH_SEPARATOR . "$dirname/../lib");
 require_once('include.php');
 
-LobbyistController::scrapeForNewLobbyActivities($argv[1]);
+if ($argv[1] == 'WAYBACK') {
+	print "archive 2014\n";
+	LobbyistController::scrapeForNewLobbyActivitiesFromTo('01-Jan-2014','01-Jan-2015');
+	print "archive 2013\n";
+	LobbyistController::scrapeForNewLobbyActivitiesFromTo('01-Jan-2013','01-Jan-2014');
+	print "archive 2012\n";
+	LobbyistController::scrapeForNewLobbyActivitiesFromTo('01-Jan-2012','01-Jan-2013');
+} else {
+	LobbyistController::scrapeForNewLobbyActivities($argv[1]);
+}
+
 LobbyistController::fixLobbyingNames();
 LobbyistController::tweetNewActivities();
 
