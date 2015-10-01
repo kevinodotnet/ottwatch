f97786a987ca8d10bfa82b6cf4d2b4c5bf958b44
Kevin O'Donnell <kevino@kevino.net>
2015-10-01 14:00:05 -0400

    remove filtering b/c category filtering fixes issues

--- www/controllers/MeetingController.php
+++ www/controllers/MeetingController.php
@@ -118,15 +118,17 @@ class MeetingController {
 				$categoryWhere 
 				and ivc1.vote in ('y','n')
 				and ivc2.vote in ('y','n')
-				and ivc1.name not in ('L. A. (Sandy) Smallwood','B. Crew','B. Padolsky','F. Malo','G. Milner','C. Quinn','S. Burt')
-				and ivc2.name not in ('L. A. (Sandy) Smallwood','B. Crew','B. Padolsky','F. Malo','G. Milner','C. Quinn','S. Burt')
-				and ivc1.name not in ('Carmelle Bédard','Douglas Wallace','J. Vézina','J.P. Kingsley','L Russell')
-				and ivc2.name not in ('Carmelle Bédard','Douglas Wallace','J. Vézina','J.P. Kingsley','L Russell')
 			group by
 				ivc1.name, ivc2.name
 			order by
 				ivc1.name, ivc2.name
 		";
+		/*
+				and ivc1.name not in ('L. A. (Sandy) Smallwood','B. Crew','B. Padolsky','F. Malo','G. Milner','C. Quinn','S. Burt')
+				and ivc2.name not in ('L. A. (Sandy) Smallwood','B. Crew','B. Padolsky','F. Malo','G. Milner','C. Quinn','S. Burt')
+				and ivc1.name not in ('Carmelle Bédard','Douglas Wallace','J. Vézina','J.P. Kingsley','L Russell')
+				and ivc2.name not in ('Carmelle Bédard','Douglas Wallace','J. Vézina','J.P. Kingsley','L Russell')
+				*/
 	
 		$pairs = array();
 	
