1d7cf838d90a199042cd81aa9f60788eb908d5bf
Kevin O'Donnell <kevino@kevino.net>
2016-02-19 13:54:50 -0500

    allow <link>

--- www/controllers/StoryController.php
+++ www/controllers/StoryController.php
@@ -230,7 +230,7 @@ class StoryController {
     <textarea id="storybody" name="body" rows="80" style=""><?php print $story['body']; ?></textarea>
     <script>
 	    $( '#storybody' ).ckeditor({
-        extraAllowedContent: 'script; table tr td th iframe pre div p blockquote {*}[*](*)',
+        extraAllowedContent: 'script; link table tr td th iframe pre div p blockquote {*}[*](*)',
 				// allowedContent: 'script b i div p blockquote a img [class][*](*){*};',
 				toolbar: [ 
 					['Source','Maximize','Save'],
