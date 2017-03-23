0567c4d2a3900282d1112830be57de214d86be1f
Kevin O'Donnell <kevino@kevino.net>
2017-03-23 08:54:40 -0400

    updated time fix on blank field in api

--- www/controllers/Ott311Controller.php
+++ www/controllers/Ott311Controller.php
@@ -192,10 +192,13 @@ class Ott311Controller {
 			'service_code' => $sr->service_code,
 			'description' => $sr->description,
 			'requested' => self::w3DateTime($sr->requested_datetime),
-			'updated' => self::w3DateTime($sr->updated_datetime),
 			'expected' => self::w3DateTime($sr->expected_datetime),
 			'address' => $sr->address
 		);
+		if ($sr->updated_datetime != '') {
+			$dbin['updated'] = self::w3DateTime($sr->updated_datetime);
+		}
+		#pr($sr);
 		$row = db_save('sr',$dbin,'sr_id');
 		# automatically close SRs if needed; this should be made to be smarter, but whatever
 		getDatabase()->execute(" update sr set close_detected = updated where close_detected is null and status = 'Closed' ");
