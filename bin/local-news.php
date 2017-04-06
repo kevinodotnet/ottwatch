fdd524116c1c5e4f2d7f797802775a7bf75cc1f7
Kevin O'Donnell <kevino@kevino.net>
2017-04-06 09:29:41 -0400

    cbc and http get for full contents

--- bin/local-news.php
+++ bin/local-news.php
@@ -15,23 +15,45 @@ if (count($argv) > 1) {
 		return;
 	}
 
+  if ($argv[1] == 'getCBC') {
+		getCBC();
+		return;
+	}
+
+	
+
+}
+
+function getCBC() {
+	$url = 'http://www.cbc.ca/cmlink/rss-canada-ottawa';
+	$xml = simplexml_load_string(file_get_contents($url), 'SimpleXMLElement', LIBXML_NOCDATA);
+	foreach ($xml->channel->item as $i) {
+		$link = $i->{'link'};
+		$linkHash = md5($link);
+		$data = file_get_contents($link);
+		$i->ottwatch_contents = $data;
+		file_put_contents(OttWatchConfig::FILE_DIR.'/localnews/cbcottawa/'.$linkHash,$i->asXML());
+	}
 }
 
 function getRss() {
 	getRssOttawaCitizen();
+	getCBC();
 }
 
 function getRssOttawaCitizen() {
 
 	# OttawaCitizen
 	$url = 'http://ottawacitizen.com/category/news/local-news/feed';
-	$xml = simplexml_load_string(c_file_get_contents($url), 'SimpleXMLElement', LIBXML_NOCDATA);
+	$xml = simplexml_load_string(file_get_contents($url), 'SimpleXMLElement', LIBXML_NOCDATA);
 
 	#pr($xml);
 
 	foreach ($xml->channel->item as $i) {
 		$link = $i->{'link'};
 		$linkHash = md5($link);
+		$data = file_get_contents($link);
+		$i->ottwatch_contents = $data;
 		file_put_contents(OttWatchConfig::FILE_DIR.'/localnews/ottawacitizen/'.$linkHash,$i->asXML());
 	}
 
