6310f26b17ee20130f7fd458121753c2011a0cc6
Kevin O'Donnell <kevino@kevino.net>
2017-09-04 21:10:47 +0000

    remove comments; fix strict open php tag

--- www/controllers/GisController.php
+++ www/controllers/GisController.php
@@ -70,29 +70,7 @@ class GisController {
 				$linecoords .= " ]\n";
 				$js .= " new google.maps.Polyline({ path: $linecoords, geodesic: true, strokeColor: '#FF0000', strokeOpacity: 1.0, strokeWeight: 2 }).setMap(map); \n";
 
-      	?>
-				<?php
-
-# var flightPlanCoordinates = [
-#     {lat: 37.772, lng: -122.214},
-#     {lat: 21.291, lng: -157.821},
-#     {lat: -18.142, lng: 178.431},
-#     {lat: -27.467, lng: 153.027}
-#   ];
-#   var flightPath = new google.maps.Polyline({
-#     path: flightPlanCoordinates,
-#     geodesic: true,
-#     strokeColor: '#FF0000',
-#     strokeOpacity: 1.0,
-#     strokeWeight: 2
-#   });
-# 
-#   flightPath.setMap(map);
-
-
-
 			}
-			#break;
 		}
 
 		?>
@@ -112,7 +90,7 @@ class GisController {
 	      });
 	    </script>
 			<script> $(document).ready(function() { <?php print $js; ?> }); </script>
-		<?
+		<?php
 
 		bottom3();
 	}
