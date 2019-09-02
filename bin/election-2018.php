ad5089e0ea6001c5a76c263e1814ce431823facb
Kevin O'Donnell <kevino@kevino.net>
2019-09-02 10:11:54 -0400

    incr

--- bin/election-2018.php
+++ bin/election-2018.php
@@ -16,10 +16,34 @@ function printCandidates() {
 }
 
 if ($argv[1] == 'createReturn') {
-	printCandidates();
-	print "enter candidate id: ";
-	$candidateid = trim(fgets(STDIN));
-	print "you chose '$candidateid'\n";
+	$file = $argv[2];
+
+	$rows = getDatabase()->all(" select id from candidate_return where filename = '$file'");
+	if (count($rows) > 0) {
+		exit(0);
+	}
+
+	preg_match('/\/([^\/]*)\.pdf/',$file,$matches);
+	$parts = split('_',$matches[1]);
+	$race = array_pop($parts);
+	$name_in = "('".join($parts,"','")."')";
+
+	$sql = " select id,ward,last,first from candidate where electionid = 5 and last in $name_in and first in $name_in ";
+	$rows = getDatabase()->all($sql);
+	if (count($rows) == 1) {
+		$candidateid = $rows[0]['id'];
+	} else if (count($rows) == 0) {
+		printCandidates();
+		print "enter candidate id: ($file)\n>> ";
+		$candidateid = trim(fgets(STDIN));
+	} else {
+		foreach ($rows as $c) {
+			print join($c,"\t")."\n";
+		}
+	}
+	print "FOUND: candidateid: $candidateid\n";
+
+	db_insert("candidate_return",array('candidateid'=>$candidateid,'filename'=>$file));
 }
 
 
