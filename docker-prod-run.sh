921248f1da8cfbc91ff7b9d8991664c4f3ea65dd
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-01-22 22:22:36 +0000

    beginnings of a deployment script

--- docker-prod-run.sh
+++ docker-prod-run.sh
@@ -2,20 +2,26 @@
 # Build the image
 #   docker build -t ottwatch-prod -f Dockerfile.prod .
 #
+# Get a shell:
+#   sudo docker exec -it ottwatch-prod /bin/bash
+#
+# Start it again:
+#   docker start -i ottwatch-prod
 
 . ~/src/infra/ottwatch.sh
 
+sudo docker stop ottwatch-web
+sudo docker container rm ottwatch-web
+
 sudo docker run \
+	-d \
 	-e RAILS_ENV=production \
 	-e DB_HOST=$DB_HOST \
 	-e DB_NAME=$DB_NAME \
 	-e DB_USER=$DB_USER \
 	-e DB_PASS=$DB_PASS \
 	-e RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
-	-p 20022:22 \
-	-p 80:3000 \
-	--name ottwatch-prod \
-	-i -t \
+	-p 3000:3000 \
+	--name ottwatch-web \
 	ottwatch-prod
 
-# docker start -i ottwatch-prod
