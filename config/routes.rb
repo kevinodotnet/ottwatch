b6b50f317f2821da8325b21afc02960eef3c9ff5
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-01-23 21:47:53 -0500

    bare bones donation review page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get '/election/donation/:id', to: 'elections#donation'
 
   # Defines the root path route ("/")
   root "home#index"
