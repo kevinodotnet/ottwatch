1e4d2495fda04fbcbfa53c7abb7393bd8297ad3c
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-02-10 17:59:00 -0500

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def google_map(center)
+    lat = center.lat
+    lon = center.lon
+    center = [lat, lon].join(',')
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
+  end
 end
