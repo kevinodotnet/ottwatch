b389b5a77b8b24e12ec743b7dba81b7b5fc3d700
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-02-11 14:23:39 -0500

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     lat = center.lat
     lon = center.lon
     center = [lat, lon].join(',')
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
+    "https://maps.googleapis.com/maps/api/staticmap?key=#{google_maps_api_key}&center=#{center}&size=300x300&zoom=13"
+  end
+
+  private
+
+  def google_maps_api_key
+    ENV["GOOGLE_MAPS_API_KEY"]
   end
 end
