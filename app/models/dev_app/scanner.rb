4a50bd1e52a43efb750e4f6ed906fd3afa2cc52c
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-02-13 16:25:25 -0500

    debugging devapp api results

--- app/models/dev_app/scanner.rb
+++ app/models/dev_app/scanner.rb
@@ -93,7 +93,9 @@ class DevApp::Scanner
       entry.save!
 
       status = data.dig("applicationStatus", "en")
+      Rails.logger.info(msg: "scanning devapp", app_number: app_number, api_status: status)
       if current_status = entry.current_status
+        Rails.logger.info(msg: "scanning devapp", app_number: app_number, api_status: status, db_status_id: current_status.id, db_status: current_status.status)
         unless current_status.status == status
           announcements << { type: :status_change, from: current_status.status, to: status}
           entry.statuses << DevApp::Status.new(status: status)
