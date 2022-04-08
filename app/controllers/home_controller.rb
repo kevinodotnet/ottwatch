ceea8ed5db40bc447104bd90ec14cc21b95e567a
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-04-08 15:50:57 -0400

    show latest announcements on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @announcements = Announcement.all.order(id: :desc).limit(50)
   end
 end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,7 +3,11 @@ class Announcement < ApplicationRecord
 
   def link_to_reference
     # TODO: this should move to a helper that can be used in the UI as well
-    url = "https://v2.ottwatch.ca"
+    url = if Rails.env.production?
+      "https://v2.ottwatch.ca"
+    else
+      "http://localhost:33000"
+    end
     url << "/devapp/#{reference.app_number}" if reference.is_a?(DevApp::Entry)
     url
   end
