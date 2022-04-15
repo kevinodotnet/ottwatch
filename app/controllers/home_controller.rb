7bca92503f1e346d15387a93bfdf8241cd944215
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-04-15 13:45:57 -0400

    avoid n+1 queries on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @announcements = Announcement.all.order(id: :desc).limit(50)
+    @announcements = Announcement.all.includes(reference: :addresses).order(id: :desc).limit(50)
   end
 end
