cd7334c86acdc6c076242b82e53ed2afdb4e9418
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-05-01 23:15:15 -0400

    get description from main page

--- app/jobs/consultation_scan_job.rb
+++ app/jobs/consultation_scan_job.rb
@@ -9,12 +9,16 @@ class ConsultationScanJob < ApplicationJob
     doc.xpath('//*[@class="project-tile__card"]').each do |d|
       href = d.xpath('a/@href').first.value
       name = d.xpath('a/div/span').first.content
-
-      puts "https://engage.ottawa.ca#{href}"
-      data = Net::HTTP.get(URI("https://engage.ottawa.ca#{href}"))
-
+      scrape_consultation(name, href)
     end
 
     nil
+  end
+
+  def scrape_consultation(name, href)
+    data = Net::HTTP.get(URI("https://engage.ottawa.ca#{href}"))
+    doc = Nokogiri::HTML(data)
+
+    description = doc.xpath('//*[@class="truncated-description"]').first.xpath('p').map{|p| p.content}.join("<br/>\n\n")
   end
 end
