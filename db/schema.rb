f3c7099748682b87560c5afbe53790444cd940ff
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-08-21 19:50:24 -0400

    add withdrew to candidates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_125853) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_234844) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_125853) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "withdrew"
     t.index ["election_id"], name: "index_candidates_on_election_id"
   end
 
