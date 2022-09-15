f61e049b87f8f08bc0dc00b11609e995b6caecb6
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-09-14 21:14:25 -0400

    configure app specific devise details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'v2.ottwatch.ca', port: 443 }
 end
