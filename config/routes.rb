8b65897b11542169a812faacc34a59ddfd70f8c6
Kevin O'Donnell <kevinodotnet@gmail.com>
2022-10-18 13:14:29 -0400

    prevent robot scanning of election data, for privacy concerns

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'parcels/:objectid', to: 'parcels#show'
-  get 'home/index'
 
-  get '/election/donation/:id', to: 'elections#donation'
+  get 'home/index'
+  root "home#index"
+
+  get 'parcels/:objectid', to: 'parcels#show'
 
   get 'devapp/index'
   get 'devapp/:app_number', to: 'devapp#show'
@@ -14,9 +15,8 @@ Rails.application.routes.draw do
   get 'meeting/index'
   get 'meeting/:reference_id', to: 'meeting#show'
 
+  get 'election/donation/:id', to: 'elections#donation'
   get 'election/legacy', to: 'election#legacy_index'
   get 'election/:id', to: 'election#show'
   get 'election/legacy/donation/:id', to: 'election#show_legacy_donation'
-  # Defines the root path rout  e ("/")
-  root "home#index"
 end
