c0ca4d335018c27bfa478424d8d0076a855d030f
Kevin O'Donnell <kevinodotnet@gmail.com>
2023-02-20 01:07:06 +0000

    fix paths to GCS keyfile in all prod docker containers

--- docker-prod-web.sh
+++ docker-prod-web.sh
@@ -8,17 +8,17 @@ sudo docker run \
   --restart always \
   -d \
   --network prodweb \
-  -e RAILS_ENV=production \
   -e DB_HOST=$DB_HOST \
-  -e DB_NAME_V1=$DB_NAME_V1 \
   -e DB_NAME=$DB_NAME \
-  -e DB_USER=$DB_USER \
+  -e DB_NAME_V1=$DB_NAME_V1 \
   -e DB_PASS=$DB_PASS \
-  -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
-  -e REDIS_URL=$REDIS_URL \
+  -e DB_USER=$DB_USER \
   -e GCS_KEYFILE=/infra/gcs-prodweb-service-account.json \
-  -e RAILS_SERVE_STATIC_FILES=1 \
   -e GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY \
+  -e RAILS_ENV=production \
+  -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
+  -e RAILS_SERVE_STATIC_FILES=1 \
+  -e REDIS_URL=$REDIS_URL \
   -e SENDGRID_API_KEY=$SENDGRID_PRODWEB_FULL \
   -e TWITTER_OAUTH_CLIENT_ID=$TWITTER_OAUTH_CLIENT_ID \
   -e TWITTER_OAUTH_CLIENT_SECRET=$TWITTER_OAUTH_CLIENT_SECRET \
