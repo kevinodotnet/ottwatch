f0d819383a45b04321cb4ecec28f914d86dd38a6
Kevin O'Donnell <kevinodotnet@gmail.com>
2023-02-20 01:11:26 +0000

    remove dead code

--- app/models/version.rb
+++
@@ -1,9 +0,0 @@
-class Version
-	def self.current
-		if File.exists?(Rails.root.join("version.json"))
-			return JSON.parse(File.read(Rails.root.join("version.json")))["object"]["sha"]
-		end
-		return Rails.env if Rails.env.development? || Rails.env.test?
-		raise StandardError.new("No version provided in production environment")
-	end
-end
