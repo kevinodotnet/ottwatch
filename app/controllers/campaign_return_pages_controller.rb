b29cc939b5a330b26ab0bf14cadda164c8a624b6
Kevin O'Donnell <kevinodotnet@gmail.com>
2023-02-26 21:21:44 -0500

    prev next navigation

--- app/controllers/campaign_return_pages_controller.rb
+++ app/controllers/campaign_return_pages_controller.rb
@@ -1,5 +1,8 @@
 class CampaignReturnPagesController < ApplicationController
   def show
     @cr_page = CampaignReturnPage.find(params[:id])
+    relation = @cr_page.campaign_return.campaign_return_pages
+    @prev_cr_page = relation.where('id < ?', @cr_page.id).order(id: :desc).limit(1).first
+    @next_cr_page = relation.where('id > ?', @cr_page.id).order(:id).limit(1).first
   end
 end
