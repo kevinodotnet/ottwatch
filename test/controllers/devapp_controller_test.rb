363c2d6bbfb508f64e5d6c0bf46cd8cb7df67e57
Kevin O'Donnell <kevinodotnet@gmail.com>
2023-12-31 16:55:53 -0500

    fix tests

--- test/controllers/devapp_controller_test.rb
+++ test/controllers/devapp_controller_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class DevappControllerTest < ActionDispatch::IntegrationTest
   test "#index works" do
-    assert_raises ActionController::RoutingError do
-      assert_equal 200, (get "/devapp/index")
-    end
+    assert_equal 200, (get "/devapp/index")
   end
 end
