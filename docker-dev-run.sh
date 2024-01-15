b86b394cdeac337730bf926de40a85ad7a244eac
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-15 08:29:40 -0500

    tidy dev helpers

--- docker-dev-run.sh
+++ docker-dev-run.sh
@@ -3,12 +3,15 @@
 # create/run the container, or if it already exists, restart the existing one
 # use 'docker container rm ottwatch-dev' to reset from the beginning
 
-echo ""
-echo ""
-echo "cd ottwatch; bin/rails db:setup"
+echo "#"
+echo "# when container is run/re-started:"
+echo "#"
 echo "/etc/init.d/mysql start"
-echo "alias m='mysql ottwatch_dev'"
-echo "mysql ottwatch_dev < db/ottwatch_v1_snapshot.sql"
+echo ""
+echo "#"
+echo "# on first run of container:"
+echo "#"
+echo "cd ottwatch; bin/rails db:setup"
 echo ""
 
 docker run --name ottwatch-dev -p 33000:3000 -v `pwd`:/ottwatch -i -t ottwatch-dev || \
