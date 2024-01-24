ef4a2a31464d8f06d317e0e81651e985015a557c
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-23 22:01:46 -0500

    bullet gem for n+1 queries

--- Gemfile
+++ Gemfile
@@ -84,6 +84,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   gem "spring"
   gem 'rubocop', require: false
+  gem 'bullet'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,15 @@ require "active_support/core_ext/integer/time"
 require "open311"
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -79,6 +88,32 @@ Rails.application.configure do
     config.api_key      = 'TODO find test key'
     config.jurisdiction = ''
   end
+
+  config.after_initialize do
+    # https://github.com/flyerhzm/bullet
+    Bullet.enable = true
+    # Bullet.sentry = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    # Bullet.xmpp = { 
+    #   :account  => 'bullets_account@jabber.org',
+    #   :password => 'bullets_password_for_jabber',
+    #   :receiver => 'your_account@jabber.org',
+    #   :show_online_status => true 
+    # }
+    Bullet.rails_logger = true
+    # # Bullet.honeybadger = true
+    # # Bullet.bugsnag = true
+    # # Bullet.appsignal = true
+    # # Bullet.airbrake = true
+    # Bullet.rollbar = true
+    Bullet.add_footer = true
+    Bullet.skip_html_injection = false
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
+    # Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+  end  
 
   # config.after_initialize do
   #   HttpLog.configure do |c|
