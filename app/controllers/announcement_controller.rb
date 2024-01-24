5fcd13163e50e899453d0308ab9ce8ea52436348
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-23 22:49:57 -0500

    bug fix; force to_a so erb variable is always an array and never a relation

--- app/controllers/announcement_controller.rb
+++ app/controllers/announcement_controller.rb
@@ -4,6 +4,6 @@ class AnnouncementController < ApplicationController
     relation = relation.where(reference_type: params[:reference_type]) if params[:reference_type]
     relation = relation.where('id < ?', params[:before_id]) if params[:before_id]
     relation = relation.limit(params[:limit] || 50).includes(:reference)
-    @announcements = relation.order(id: :desc)
+    @announcements = relation.order(id: :desc).to_a
   end
 end
