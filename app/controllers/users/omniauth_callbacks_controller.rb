5761ca58f1bb4d15bc22d60303db1d2584a84171
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-24 21:14:27 -0500

    add kind back

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    generic_handler
+    generic_handler("google")
   end
 
   def github
-    generic_handler
+    generic_handler("github")
   end
 
-  def generic_handler
+  def generic_handler(kind)
     Rails.logger.info("omniauth.auth: #{request.env["omniauth.auth"]}")
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-    set_flash_message(:notice, :success) if is_navigational_format?
+    set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
   end
 
   def failure
