1a251df0a36a372a9bed7fed42a03abe916c25a7
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-24 21:10:20 -0500

    all oauth users are persisted anyway

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,12 +11,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     Rails.logger.info("omniauth.auth: #{request.env["omniauth.auth"]}")
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "google?") if is_navigational_format?
-    else
-      redirect_to new_user_registration_url
-    end
+    sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success) if is_navigational_format?
   end
 
   def failure
