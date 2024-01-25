2cfab7b217e36fc8a18fb5bc17c4e73e85d81899
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-01-24 21:30:39 -0500

    log user data after login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,6 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def generic_handler(kind)
     Rails.logger.info("omniauth.auth: #{request.env["omniauth.auth"]}")
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    Rails.logger.info(msg: "user", user_id: @user.id, user_email: @user.email, provider: @user.provider, uuid: @user.uid, persisted: @user.persisted?)
 
     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
     set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
