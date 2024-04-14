813ab799e8c431bd4983124c679fde3851d0b38c
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-04-14 17:26:23 -0400

    full rebuild dev

--- docker-dev-build.sh
+++ docker-dev-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # --progress=plain
+#
+# BUILD_NO_CACHE=--no-cache ./docker-dev-build.sh
 
-docker build -t ottwatch-base -f Dockerfile.base . && \
-  docker build -t ottwatch-dev -f Dockerfile.dev .
+docker build $BUILD_NO_CACHE -t ottwatch-base -f Dockerfile.base . && \
+  docker build $BUILD_NO_CACHE -t ottwatch-dev -f Dockerfile.dev .
