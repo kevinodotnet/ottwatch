a6b1aed2417825c2e4cee528030e985fd365d134
Kevin O'Donnell <kevinodotnet@gmail.com>
2024-05-27 01:48:42 +0100

    spin up dev containers at will

--- docker/dev-run.sh
+++ docker/dev-run.sh
@@ -3,17 +3,28 @@
 # create/run the container, or if it already exists, restart the existing one
 # use 'docker container rm ottwatch-dev' to reset from the beginning
 
-echo "#"
-echo "# when container is run/re-started:"
-echo "#"
-echo "/etc/init.d/mysql start"
-echo ""
-echo "#"
-echo "# on first run of container:"
-echo "#"
-echo "cd ottwatch; bin/rails db:setup"
-echo ""
+cat << EOF
+#
+# when container is run/re-started:
+#
+/etc/init.d/mysql start
 
-docker run --name ottwatch-dev -p 33000:3000 -v `pwd`/..:/ottwatch -i -t ottwatch-dev || \
-  docker start -i ottwatch-dev
+#
+# on first run of container:
+#
+cd ottwatch; bin/rails db:setup
+EOF
+
+instance=$1
+if [ -z "${instance}" ]; then
+  instance="ottwatch-dev"
+fi
+
+port=$2
+if [ -z "${port}" ]; then
+  port=33000
+fi
+
+docker run --name $instance -p $port:3000 -v `pwd`/..:/ottwatch -i -t ottwatch-dev || \
+  docker start -i $instance
 
