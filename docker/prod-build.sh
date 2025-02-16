9e9bb5d14fe2c0eeccfddd7853a6d6f1c1732625
Kevin O'Donnell <kevinodotnet@gmail.com>
2025-02-16 18:03:10 +0000

    no cache on main prod image either

--- docker/prod-build.sh
+++ docker/prod-build.sh
@@ -2,6 +2,6 @@
 
 . ~/src/infra/ottwatch.sh
 
-sudo docker build -t ottwatch-base -f Dockerfile.base .
+sudo docker build $BUILD_NO_CACHE -t ottwatch-base -f Dockerfile.base .
 sudo docker build $BUILD_NO_CACHE -t ottwatch-prod -f Dockerfile.prod .
 sudo docker image prune -f
