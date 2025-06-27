f4a1380c204651aea94595210913feb66551e78c
Kevin O'Donnell <kevinodotnet@gmail.com>
2025-06-27 19:41:13 -0400

    revert sqlite dual writes

--- app/models/traffic_camera.rb
+++ app/models/traffic_camera.rb
@@ -1,7 +1,4 @@
 class TrafficCamera < ApplicationRecord
-    CAPTURE_FOLDER = (ENV["LOCAL_STORAGE_FOLDER"] || Rails.root.join("tmp").to_s) + "/camera"
-    SQLITE_ARCHIVE = "#{CAPTURE_FOLDER}/camera_archive.sqlar"
-
     def self.cameras
         @cameras ||= begin
             data = Net::HTTP.get(URI("https://traffic.ottawa.ca/service/camera"))
@@ -22,6 +19,11 @@ class TrafficCamera < ApplicationRecord
         end
     end
 
+    def self.capture_folder
+        dir = ENV["LOCAL_STORAGE_FOLDER"] || Rails.root.join("tmp").to_s
+        "#{dir}/camera"
+    end
+
     def current_image_url
         time_now = (Time.now.to_f * 1000).to_i
         "https://traffic.ottawa.ca/camera?id=#{camera_number}&timems=#{time_now}"
@@ -30,39 +32,11 @@ class TrafficCamera < ApplicationRecord
     def capture_image
         time_now = (Time.now.to_f * 1000).to_i
         response = Net::HTTP.get(URI(current_image_url))
-        camera_path = "#{CAPTURE_FOLDER}/#{id}"
+        camera_path = "#{self.class.capture_folder}/#{id}"
         capture_filename = "#{camera_path}/#{id}_#{time_now}.jpg"
         FileUtils.mkdir_p(camera_path)
         File.binwrite(capture_filename, response)
-        save_image_to_sqlite_archive(time: time_now, image: response)
-        {
-            time: time_now,
-            camera_id: id,
-            filename: capture_filename,
-            image: response
-        }
-    end
-
-    def save_image_to_sqlite_archive(time:, image:)
-        filename = "#{id}_#{time}.jpg"
-        file_data = nil # File.binread(filename)
-        file_stat = nil # File.stat(filename)
-
-        SQLite3::Database.open(SQLITE_ARCHIVE) do |db|
-            db.execute(
-                "INSERT INTO sqlar (name, mode, mtime, sz, data) VALUES (?, ?, ?, ?, ?)",
-                [filename, file_stat&.mode, file_stat&.mtime.to_i, image.size, image]
-            )
-        end
-    end
-
-    def image_from_sqlite_archive(time_ms)
-        SQLite3::Database.open(SQLITE_ARCHIVE) do |db|
-            db.execute("SELECT data FROM sqlar WHERE name = ?", ["#{id}_#{time_ms}.jpg"]) do |row|
-                return row.first
-            end
-        end
-        nil
+        response
     end
 
     def capture_jpg(time_ms)
@@ -72,7 +46,7 @@ class TrafficCamera < ApplicationRecord
     end
 
     def captures 
-        Dir.glob(File.join(CAPTURE_FOLDER, id.to_s, '**', '*')).select { |f| File.file?(f) }.sort.map do |file|
+        Dir.glob(File.join(self.class.capture_folder, id.to_s, '**', '*')).select { |f| File.file?(f) }.sort.map do |file|
             time_ms = file.scan(/.*#{id}\/#{id}_(\d+)\.jpg/).first.first.to_i
             time = time_ms / 1000
             {
