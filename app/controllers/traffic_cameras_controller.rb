290b30b4469d602e47a1e29e0ab0dd53f9343aea
Kevin O'Donnell <kevinodotnet@gmail.com>
2025-07-12 23:20:00 -0400

    handle html requests from bots

--- app/controllers/traffic_cameras_controller.rb
+++ app/controllers/traffic_cameras_controller.rb
@@ -13,7 +13,14 @@ class TrafficCamerasController < ApplicationController
     @traffic_camera = TrafficCamera.find(params[:id])
     capture = @traffic_camera.captures.detect{|c| c[:time_ms] == params[:time_ms].to_i}
     return unless capture
-    response.headers['Cache-Control'] = 'public, max-age=86400'
-    send_data(File.read(capture[:file]), type: 'image/jpeg', disposition: 'inline', filename: "cam_#{params[:id]}_#{params[:time_ms]}.jpg")
+    
+    respond_to do |format|
+      format.jpeg do
+        response.headers['Cache-Control'] = 'public, max-age=86400'
+        send_data(File.read(capture[:file]), type: 'image/jpeg', disposition: 'inline', filename: "cam_#{params[:id]}_#{params[:time_ms]}.jpg")
+      end
+      format.html { redirect_to traffic_camera_path(@traffic_camera) }
+      format.any { head :not_acceptable }
+    end
   end
 end
