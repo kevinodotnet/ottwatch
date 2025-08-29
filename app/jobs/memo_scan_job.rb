1244ce0e08ac682c5fb8883427a5a8ac32aed5b4
Kevin O'Donnell <kevinodotnet@gmail.com>
2025-08-28 23:41:23 -0400

    scan page and enqueue jobs

--- app/jobs/memo_scan_job.rb
+++ app/jobs/memo_scan_job.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'uri'
+require 'selenium-webdriver'
 
 class MemoScanJob < ApplicationJob
   queue_as :default
@@ -18,37 +19,40 @@ class MemoScanJob < ApplicationJob
     
     memo_links = doc.css("a[href^=\"#{BASE_URL.sub('https://ottawa.ca', '')}\"]")
     
-    memo_links.each do |link|
-      puts link
+    pages = memo_links.map do |link|
+      # href to hash
+      href = link.attributes["href"].value
+      next unless href.match?(/#section/)
+      "https://ottawa.ca#{href.gsub(/#.*/, '')}"
+    end.compact.uniq
+
+    pages.each do |p|
+      MemoScanJob.set(wait: rand(100..500).seconds).perform_later(page: p)
     end
   end
 
   def fetch_page(url)
-    uri = URI(url)
-    req = Net::HTTP::Get.new(uri)
-    
-    req['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
-    req['Accept-Language'] = 'en-US,en;q=0.9'
-    req['Cache-Control'] = 'no-cache'
-    # req['Connection'] = 'keep-alive'
-    # req['Pragma'] = 'no-cache'
-    req['Sec-Fetch-Dest'] = 'document'
-    req['Sec-Fetch-Mode'] = 'navigate'
-    req['Sec-Fetch-Site'] = 'none'
-    req['Sec-Fetch-User'] = '?1'
-    req['Upgrade-Insecure-Requests'] = '1'
-    req['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
-    req['sec-ch-ua'] = '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"'
-    req['sec-ch-ua-mobile'] = '?0'
-    req['sec-ch-ua-platform'] = '"macOS"'
-    
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    
-    response = http.request(req)
-    response.body
+   uri = URI(url)
+   req = Net::HTTP::Get.new(uri)
+   req['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+   req['Accept-Language'] = 'en-US,en;q=0.9'
+   req['Cache-Control'] = 'no-cache'
+   # req['Connection'] = 'keep-alive'
+   # req['Pragma'] = 'no-cache'
+   req['Sec-Fetch-Dest'] = 'document'
+   req['Sec-Fetch-Mode'] = 'navigate'
+   req['Sec-Fetch-Site'] = 'none'
+   req['Sec-Fetch-User'] = '?1'
+   req['Upgrade-Insecure-Requests'] = '1'
+   req['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
+   req['sec-ch-ua'] = '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"'
+   req['sec-ch-ua-mobile'] = '?0'
+   req['sec-ch-ua-platform'] = '"macOS"'
+   http = Net::HTTP.new(uri.host, uri.port)
+   http.use_ssl = true
+   response = http.request(req)
+   response.body
   end
-
 
   def create_announcement(memo)
     memo.announcements.create!(
--- test/jobs/memo_scan_job_test.rb
+++ test/jobs/memo_scan_job_test.rb
@@ -1,10 +1,15 @@
 require "test_helper"
 
 class MemoScanJobTest < ActiveJob::TestCase
-  test "main memo page gets parsed" do
+  test "perform with no args scans main memo page and enqueues jobs" do
     VCR.use_cassette("#{class_name}_#{method_name}") do
       MemoScanJob.perform_now
-      assert 1 == 1
+
+      assert enqueued_jobs.select { |job| job[:job] == MemoScanJob}.count > 10
+
+      enqueued_jobs.select { |job| job[:job] == MemoScanJob }.each do |job|
+        assert job[:args].first.key?("page"), "Expected job to have page: argument"
+      end
     end
   end
 end
